03d9200b8579550885dc10884159eb18aed16045
Fai Wong <wongwf82@gmail.com>
2013-09-06 01:19:50 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Endorsely::Application.config.secret_token = '4e69ad1b5d41f127b432f10f81a6c686b38673085ddb5e86b1d83ca34801387d80b7236b0622a89222389bd5347a38ae13dc225f1118a8c72b8d63dbae4dc143'
